f6e8beaaea913b558c94a2382b596b575552e612
enigmaiiiiiiii <62017693+enigmaiiiiiiii@users.noreply.github.com>
2022-10-27 23:01:10 +0800

    add some code

---
+++ code.ts
@@ -0,0 +1,14 @@
+let helloWorld = "Hello World";
+
+interface User {
+    name: string;
+    id: number;
+}
+
+const user: User = {
+    name: "John",
+    id: 0,
+};
+
+
+console.log('Hello World');
---
+++ tswrongsyntax.ts
@@ -0,0 +1,10 @@
+
+interface User {
+    name: string;
+    id: number;
+}
+
+const user2: User = {
+    username: "John",
+    id: 0,
+}
