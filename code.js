a5e59df8d59ffd850264398a0b6eb01f42c8cdbd
enigmaiiiiiiii <62017693+enigmaiiiiiiii@users.noreply.github.com>
2023-01-28 10:07:14 +0800

    fix

--- code.js
+++ code.js
@@ -7,7 +7,5 @@ const gen = generator(10);
 
 console.log(gen.next().value);
 console.log(gen.next().value);
-console.log(gen.next().value);
 
 const GeneratorFunction = function* () { }.constructor;
-const g = new GeneratorFunction("a", "yield  a * 2");
