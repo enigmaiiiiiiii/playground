2e0bf199723d851e154985f5179cd9d9fdfb34f3
enigmaiiiiiiii <62017693+enigmaiiiiiiii@users.noreply.github.com>
2023-02-06 17:05:33 +0800

    rename with script

---
+++ newdemo.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+// Rename the file
+fs.renameSync('demo.js', 'newdemo.js');
+
+// Tell Git about the rename
+execSync('git mv demo.js newdemo.js');
