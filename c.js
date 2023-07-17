d0539e4b7c4be11fdf1a4ccaa73752565190d33a
enigmaiiiiii <enigmaiiiiii@localhost.localdomain>
2023-07-17 10:58:46 +0800

    main update

--- c.js
+++
@@ -1,2 +0,0 @@
-console.log("hello f.js")
-
--- newscript.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
-
-execSync(`git mv b.js c.js`);
-
-console.log('another change')
