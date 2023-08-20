3cc2aef4facd9b69d5dc5ed302eb8a3f079808f3
enigmaiiiiii <enigmaiiiiii@localhost.localdomain>
2023-08-20 20:52:23 +0800

    2052

--- python/test.py
+++ python/test.py
@@ -1,10 +1,10 @@
-# quick sort
-def quick_sort(l: list):
-    if len(l) <= 1:
-        return l
-    else:
-        pivot = l[0]
-        less = [i for i in l[1:] if i <= pivot]
-        greater = [i for i in l[1:] if i > pivot]
-        return quick_sort(less) + [pivot] + quick_sort(greater)
+class Generator:
+    pass
 
+class DopamineGenerator:
+    def __init__(self, config):
+        self.config = config
+        self.generator = Generator(config)
+
+    def generate(self):
+        self.generator.generate()
